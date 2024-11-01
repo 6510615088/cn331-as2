01f5b053ac0fd51f20a1d6d86ebef49c80b488f8
Napat Wongkaew <6510615070@cn330>
2024-11-01 15:22:48 +0700

    test for subjectreg

--- subjectreg/tests.py
+++ subjectreg/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Subject
 
-# Create your tests here.
+class SubjectModelTest(TestCase):
+    # Test for adding a subject to the model
+    def test_subject_creation(self):
+        subject = Subject.objects.create(
+            subject_id='CN101',
+            name='Introduction to Computer Programming',
+            semester=1,
+            academic_year=2024,
+            max_students=2,
+            open_for_registration=True
+        )
+        self.assertEqual(subject.name, 'Introduction to Computer Programming')
+
+    # Test for a client can reach a homepage or not
+    def test_homepage(self):
+        c = Client()
+        response = c.get(reverse('homepage'))
+        self.assertEqual(response.status_code, 200)
+        
